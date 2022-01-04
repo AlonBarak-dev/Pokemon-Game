e48426d411106ff00166d1e44bdff242f4c12f6e
alonbarak <alonbarak3110@gmail.com>
2022-01-04 22:10:05 +0200

    added Button class for GUI

--- client_python/student_code.py
+++ client_python/student_code.py
@@ -4,8 +4,9 @@ OOP - Ex4
 Very simple GUI example for python client to communicates with the server and "play the game!"
 """
 import math as mh
+import socket
+import sys
 from threading import Thread
-from types import SimpleNamespace
 from client import Client
 import json
 from pygame import gfxdraw
@@ -14,8 +15,8 @@ from pygame import *
 from client_python.Agent import Agent
 from client_python.Info import Info
 from client_python.pokemon import Pokemon
-from graph.DiGraph import DiGraph
 from graph.GraphAlgo import GraphAlgo
+from Button import Button
 
 # init pygame
 WIDTH, HEIGHT = 1080, 720
@@ -89,7 +90,6 @@ def run_agent(agent: Agent, g_algo: GraphAlgo):
             '{"agent_id":' + str(agent.id) + ', "next_node_id":' + str(agent.path[1 % len(agent.path)]) + '}')
         agent.pos = g_algo.get_graph().nodes[agent.path[1 % len(agent.path)]].pos
         agent.path.remove(agent.path[0])
-        print(agent.dest)
         if isinstance(g_algo.get_graph().get_all_v()[p], Pokemon):
             client.move()
             return
@@ -125,7 +125,6 @@ def find_nearest_avaliable_agent(agent_list: list, curr_pokemon: Pokemon, graph:
                 agent_res = agent
 
         agent_res.path = path  # update the agent path
-        print(path)
         return agent_res.id  # return the agent id
 
     else:  # in case there are no free agents, loop over the agents
@@ -149,133 +148,139 @@ def find_nearest_avaliable_agent(agent_list: list, curr_pokemon: Pokemon, graph:
         return agent_res.id
 
 
-while client.is_running() == 'true':
+try:
+    while client.is_running() == 'true':
 
-    graph_copy = GraphAlgo()  # copy graph
-    graph_copy.load_from_json(json.loads(graph_json))
+        graph_copy = GraphAlgo()  # copy graph
+        graph_copy.load_from_json(json.loads(graph_json))
 
-    # initialize pokemon list
-    pokemons = json.loads(client.get_pokemons())
-    pokemon_list = []
-    pokemons = pokemons.get("Pokemons")
+        # initialize pokemon list
+        pokemons = json.loads(client.get_pokemons())
+        pokemon_list = []
+        pokemons = pokemons.get("Pokemons")
 
-    for pokemon in pokemons:
-        key = max(graph_copy.get_graph().nodes.keys()) + 1
-        poki = Pokemon.from_dict_pok(pokemon.get("Pokemon"), key)
-        graph_copy.graph.add_pokemon(poki)
+        for pokemon in pokemons:
+            key = max(graph_copy.get_graph().nodes.keys()) + 1
+            poki = Pokemon.from_dict_pok(pokemon.get("Pokemon"), key)
+            graph_copy.graph.add_pokemon(poki)
 
-        edge, weight1, weight2 = graph.graph.find_edge(poki)
-        src = edge.src
-        dest = edge.dest
-        weight = edge.weight
-        poki.edge = edge
-        graph_copy.graph.add_edge(src, poki.key, weight1)
-        graph_copy.graph.add_edge(poki.key, dest, weight2)
-        graph_copy.graph.remove_edge(edge.src, edge.dest)
+            edge, weight1, weight2 = graph.graph.find_edge(poki)
+            src = edge.src
+            dest = edge.dest
+            weight = edge.weight
+            poki.edge = edge
+            graph_copy.graph.add_edge(src, poki.key, weight1)
+            graph_copy.graph.add_edge(poki.key, dest, weight2)
+            graph_copy.graph.remove_edge(edge.src, edge.dest)
 
-        pokemon_list.append(poki)
-        print(poki.type)
+            pokemon_list.append(poki)
+            print(poki.type)
 
-    print(graph_copy.graph)
-    # creating agent object from the json file
-    agent_json = json.loads(client.get_agents())
-    agents = []
-    agent_json = agent_json.get("Agents")
-    for agent in agent_json:
-        agt = Agent.from_dict(agent.get("Agent"))
-        agents.append(agt)
+        print(graph_copy.graph)
+        # creating agent object from the json file
+        agent_json = json.loads(client.get_agents())
+        agents = []
+        agent_json = agent_json.get("Agents")
+        for agent in agent_json:
+            agt = Agent.from_dict(agent.get("Agent"))
+            agents.append(agt)
 
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ GUI
-    # check events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit(0)
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ GUI
+        # check events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit(0)
 
-    # refresh surface
-    screen.fill(Color(0, 0, 0))
+        # refresh surface
+        screen.fill(Color(0, 0, 0))
 
-    # draw nodes
-    for n in graph.get_graph().nodes.values():
-        x = my_scale(n.pos.x, x=True)
-        y = my_scale(n.pos.y, y=True)
-        # its just to get a nice antialiased circle
-        gfxdraw.filled_circle(screen, int(x), int(y),
-                              radius, Color(64, 80, 174))
-        gfxdraw.aacircle(screen, int(x), int(y),
-                         radius, Color(255, 255, 255))
+        # draw nodes
+        for n in graph.get_graph().nodes.values():
+            x = my_scale(n.pos.x, x=True)
+            y = my_scale(n.pos.y, y=True)
+            # its just to get a nice antialiased circle
+            gfxdraw.filled_circle(screen, int(x), int(y),
+                                  radius, Color(64, 80, 174))
+            gfxdraw.aacircle(screen, int(x), int(y),
+                             radius, Color(255, 255, 255))
 
-        # draw the node id
-        id_srf = FONT.render(str(n.key), True, Color(255, 255, 255))
-        rect = id_srf.get_rect(center=(x, y))
-        screen.blit(id_srf, rect)
+            # draw the node id
+            id_srf = FONT.render(str(n.key), True, Color(255, 255, 255))
+            rect = id_srf.get_rect(center=(x, y))
+            screen.blit(id_srf, rect)
 
-    # draw edges
-    for e in graph.get_graph().edges:
-        # find the edge nodes
-        src = next(n for n in graph.get_graph().nodes.values() if n.key == e.src)
-        dest = next(n for n in graph.get_graph().nodes.values() if n.key == e.dest)
+        # draw edges
+        for e in graph.get_graph().edges:
+            # find the edge nodes
+            src = next(n for n in graph.get_graph().nodes.values() if n.key == e.src)
+            dest = next(n for n in graph.get_graph().nodes.values() if n.key == e.dest)
 
-        # scaled positions
-        src_x = my_scale(src.pos.x, x=True)
-        src_y = my_scale(src.pos.y, y=True)
-        dest_x = my_scale(dest.pos.x, x=True)
-        dest_y = my_scale(dest.pos.y, y=True)
+            # scaled positions
+            src_x = my_scale(src.pos.x, x=True)
+            src_y = my_scale(src.pos.y, y=True)
+            dest_x = my_scale(dest.pos.x, x=True)
+            dest_y = my_scale(dest.pos.y, y=True)
 
-        # draw the line
-        pygame.draw.line(screen, Color(61, 72, 126),
-                         (src_x, src_y), (dest_x, dest_y))
+            # draw the line
+            pygame.draw.line(screen, Color(61, 72, 126),
+                             (src_x, src_y), (dest_x, dest_y))
 
-    # draw agents
-    for agent in agents:
-        x = my_scale(float(agent.pos.x), x=True)
-        y = my_scale(float(agent.pos.y), y=True)
-        pygame.draw.circle(screen, Color(122, 61, 23), (x, y), 10)
-
-    # draw Pokemons (note: should differ (GUI wise) between the up and the down pokemons (currently they are marked
-    # in the same way).
-
-    for p in pokemon_list:
-        x = my_scale(float(p.pos.x), x=True)
-        y = my_scale(float(p.pos.y), y=True)
-        pygame.draw.circle(screen, Color(0, 255, 255), (x, y), 10)
-
-    # update screen changes
-    display.update()
-
-    # refresh rate
-    clock.tick(10)
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ GUI
-
-    pokemon_list = sorted(pokemon_list, key=sorting_func, reverse=True)  # sort the pockemons base on their values
-    # assign agent for each pokemon
-    for pokemon in pokemon_list:
-        agent_id: int = find_nearest_avaliable_agent(agents, pokemon, graph_copy)
-        pokemon.agent_id = agent_id
-
-    stop = False
-    if len(agents) > 1:
+        # draw agents
         for agent in agents:
-            run_agent(agent, graph_copy)
-    else:
-        busy_agents = []
-        for agent in agents:
-            if len(agent.path) != 0:
-                busy_agents.append(agent)
+            x = my_scale(float(agent.pos.x), x=True)
+            y = my_scale(float(agent.pos.y), y=True)
+            pygame.draw.circle(screen, Color(122, 61, 23), (x, y), 10)
 
-        threads = []
+        # draw Pokemons (note: should differ (GUI wise) between the up and the down pokemons (currently they are marked
+        # in the same way).
 
-        for agent in busy_agents:
-            thread = Thread(target=run_agent, args=(agent, graph_copy))
-            threads.append(thread)
-            thread.start()
+        for p in pokemon_list:
+            x = my_scale(float(p.pos.x), x=True)
+            y = my_scale(float(p.pos.y), y=True)
+            pygame.draw.circle(screen, Color(0, 255, 255), (x, y), 10)
 
-        i = 0
-        while threads[i % len(threads)].is_alive():
-            i += 1
+       # stop_button = Button()
 
-        stop = True
-        for thread in threads:
-            thread.join()
+        # update screen changes
+        display.update()
+
+        # refresh rate
+        clock.tick(10)
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ GUI
+
+        pokemon_list = sorted(pokemon_list, key=sorting_func, reverse=True)  # sort the pockemons base on their values
+        # assign agent for each pokemon
+        for pokemon in pokemon_list:
+            agent_id: int = find_nearest_avaliable_agent(agents, pokemon, graph_copy)
+            pokemon.agent_id = agent_id
+
+        stop = False
+        if len(agents) > 1:
+            for agent in agents:
+                run_agent(agent, graph_copy)
+        else:
+            busy_agents = []
+            for agent in agents:
+                if len(agent.path) != 0:
+                    busy_agents.append(agent)
+
+            threads = []
+
+            for agent in busy_agents:
+                thread = Thread(target=run_agent, args=(agent, graph_copy))
+                threads.append(thread)
+                thread.start()
+
+            i = 0
+            while threads[i % len(threads)].is_alive():
+                i += 1
+
+            stop = True
+            for thread in threads:
+                thread.join()
+except socket.error:
+    print("the server is down!")
+    sys.exit(1)
 
 # game over:
